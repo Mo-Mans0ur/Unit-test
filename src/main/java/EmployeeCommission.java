cd3e7f733b0b12182160b6ea6e9cf17aa347600f
Mo-Mans0ur <Moha1998@outlook.dk>
2022-04-25 19:37:00 +0200

    exercise 2 done

---
+++ src/main/java/EmployeeCommission.java
@@ -0,0 +1,13 @@
+public class EmployeeCommission {
+/*
+    public boolean commission(Object employeeCommission) {
+        int baseSalary = 120;
+        if (employeeCommission baseSalary) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+ */
+}
